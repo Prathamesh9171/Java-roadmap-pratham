7584d83d5e6156704f7479f17e590111c7270f63
Prathamesh9171 <kashidprathamesh8@gmail.com>
2026-01-26 18:39:07 +0530

    Day1: Java setup + basic program
    Learning java + daily practice code

---
+++ Week-01-Java-Basics/Day-01/PrintDetails.java
@@ -0,0 +1,7 @@
+public class PrintDetails {
+    public static void main(String[] args) {
+        System.out.println("Name:Pratham");
+        System.out.println("Branch:ECE");
+        System.out.println("College:Pillai HOC College");
+    }
+}
