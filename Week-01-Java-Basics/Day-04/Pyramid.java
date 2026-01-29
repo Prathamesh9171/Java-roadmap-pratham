488652d75925551cf0b8f18973ffa6d1dc1d5464
Prathamesh9171 <kashidprathamesh8@gmail.com>
2026-01-29 19:49:57 +0530

    Day-04:nested loops and basics patterns

---
+++ Week-01-Java-Basics/Day-04/Pyramid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an number: ");
+        int n = sc.nextInt();
+
+         for (int i = 1; i <=n; i++) {
+            for (int j = i; j <= n-1 ; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <=(2*i-1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
