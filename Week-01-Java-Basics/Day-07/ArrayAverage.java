b3f4f6ed0237c27d26ed53d11beb58d86bb29721
Prathamesh9171 <kashidprathamesh8@gmail.com>
2026-02-01 18:57:07 +0530

    Day7: Arrays program

---
+++ Week-01-Java-Basics/Day-07/ArrayAverage.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ArrayAverage {
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.print("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i]; 
+        }
+        double avg  = (double) sum / n;
+        System.out.println("Average: "+ avg);
+
+        sc.close();
+    }
+}
