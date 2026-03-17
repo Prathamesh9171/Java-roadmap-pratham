daa5fc548b11d874ba18a546b0960abe4d23ad47
Prathamesh9171 <kashidprathamesh8@gmail.com>
2026-03-17 20:56:36 +0530

    feat: added database indexing for performance optimization

---
+++ Week-07-Performance/Day-3-Database-Optimization/studentapi/src/main/java/com/pratham/studentapi/StudentapiApplication.java
@@ -0,0 +1,17 @@
+package com.pratham.studentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableCaching
+@EnableAsync
+public class StudentapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentapiApplication.class, args);
+	}
+
+}
