b021bcb9ff748f8efdd033a2f2c5ba1a2b3599c6
Prathamesh9171 <kashidprathamesh8@gmail.com>
2026-03-19 18:20:43 +0530

    feat: implemented dynamic filtering with pagination using jpa specification

---
+++ Week-07-Performance/Day-5-Advanced-Filtering/studentapi/src/main/java/com/pratham/studentapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.pratham.studentapi.exception;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+
+        String errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
+
+
